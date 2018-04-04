28f9e334704e23f3517a0e9046ddbba582996050
angelloro <Angel.Loro@alu.uclm.es>
2018-04-04 23:45:00 +0200

    Grafo

--- src/practicapd/PracticaPD.java
+++ src/practicapd/PracticaPD.java
@@ -15,32 +15,27 @@ public class PracticaPD {
         int num = 24;
         ArrayList pa = primosDivisores(num);
         ArrayList<Numero> vertices = sacarVertices(num);
-        for(int i = 0 ; i<vertices.size() ; i++){    
+        Numero raiz = new Numero(num, division(num, pa), null, 0);
+        vertices.add(0, raiz);
+
+        mapa = new Grafo(vertices.size());
+        mapa=editarGrafo(mapa,vertices);
+        
+         leer.p(mapa.toString());
+
+        /*for(int i = 0 ; i<vertices.size() ; i++){    
             leer.pln(""+vertices.get(i).getNumero());
             
-        }
-
-        mapa = new Grafo(1000);
-        Numero raiz=new Numero(num,pa,null,0);        
-        forward(raiz);
-        
-        for(int i = 0 ; i < raiz.getPrimos().size() ; i++){
-            int a = (int)raiz.getPrimos().get(i);
-            Numero n = new Numero(a,primosDivisores(a),raiz,0);
-            mapa.nuevoVertice(n);
-            
-        }
+        }*/
+        //forward(raiz);
     }
 
     public static void forward(Numero raiz) {
-        
+
         ArrayList<Numero> anchura = new ArrayList<Numero>();
         raiz.setCoste(0);
         anchura.add(raiz);
-        
-       
 
-        
         leer.pln(mapa.toString());
         ArrayList<Numero> numeros = mapa.vertices();
         int c = 0;
@@ -67,7 +62,7 @@ public class PracticaPD {
 
     public static ArrayList<Integer> primosMenores(int num) {
         ArrayList<Integer> primos = new ArrayList();
-        
+
         primos.add(2);
         primos.add(3);
         primos.add(5);
@@ -78,16 +73,16 @@ public class PracticaPD {
         }
         return primos;
     }
-    
-    public static ArrayList<Integer> primosDivisores(int num){
+
+    public static ArrayList<Integer> primosDivisores(int num) {
         ArrayList<Integer> primos = primosMenores(num);
         ArrayList<Integer> aux = new ArrayList();
         int cont;
-       
+
         for (int i = 0; i < primos.size(); i++) {
             cont = 1;
             while (num % primos.get(i) == 0) {
-                aux.add((int)Math.pow(primos.get(i), cont));
+                aux.add((int) Math.pow(primos.get(i), cont));
                 num /= primos.get(i);
                 cont++;
             }
@@ -95,32 +90,59 @@ public class PracticaPD {
         primos = aux;
         return primos;
     }
-    
-    public static ArrayList<Integer> division(int num, ArrayList<Integer> primos){
+
+    public static ArrayList<Integer> division(int num, ArrayList<Integer> primos) {
         ArrayList<Integer> aux = new ArrayList<Integer>();
-        for(int i = 0 ; i < primos.size() ; i++){
-            aux.add(num/primos.get(i));
+        for (int i = 0; i < primos.size(); i++) {
+            aux.add(num / primos.get(i));
         }
         return aux;
     }
-    
-    public static ArrayList<Numero> sacarVertices(int num){
+
+    public static ArrayList<Numero> sacarVertices(int num) {
         int numeroVer;
         ArrayList<Integer> nVer = division(num, primosDivisores(num));
         ArrayList<Integer> aux = new ArrayList<Integer>();
         ArrayList<Numero> nas = new ArrayList<Numero>();
-        for(int i = 0 ; i < nVer.size() ; i++){
-            aux = division(nVer.get(i),primosDivisores(nVer.get(i)));
-            Numero n = new Numero(nVer.get(i),aux,null,0);               
-            nas.add(n);                    
-            for(int j = 0 ; j < aux.size() ; j++){
-                if(!nVer.contains(aux.get(j))){
+        for (int i = 0; i < nVer.size(); i++) {
+            aux = division(nVer.get(i), primosDivisores(nVer.get(i)));
+            Numero n = new Numero(nVer.get(i), aux, null, 0);
+            nas.add(n);
+            for (int j = 0; j < aux.size(); j++) {
+                if (!nVer.contains(aux.get(j))) {
                     nVer.add(aux.get(j));
                 }
             }
         }
-        nVer.add(0,num);
-        
+        nVer.add(0, num);
+
         return nas;
+    }
+    
+    public static Grafo editarGrafo(Grafo mapa,ArrayList<Numero> vertices){
+                for (int i = 0; i < vertices.size(); i++) {
+            Numero a = vertices.get(i);
+            //Numero n = new Numero(a,primosDivisores(a),raiz,0);
+            mapa.nuevoVertice(a);
+
+        }
+        ArrayList<Numero> V = mapa.vertices();
+        for (int x = 0; x < V.size(); x++) {
+            Numero O = V.get(x);
+            Numero X = null;
+            ArrayList<Integer> Prim = O.getPrimos();
+            for (int y = 0; y < Prim.size(); y++) {
+                for (int z = 0; z < V.size(); z++) {
+                    Numero N1 = V.get(z);
+                    if (N1.getNumero() == Prim.get(y)) {
+                        X = N1;
+                    }
+                }
+                
+                mapa.nuevoArco(O, X, 0);
+            }
+            
+        }
+        return mapa;
     }
 }
